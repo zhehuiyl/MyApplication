c62baae85f4c61337a591d6fb33cf9fa6f952f64
zhehuiyl <zhehuiyl@163.com>
2016-11-20 16:04:48 +0800

    First commit to GitHub

---
+++ app/src/main/java/com/example/yanle/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.yanle.myapplication;
+
+import android.app.Dialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
